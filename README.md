042aa44e288d6fe6bffaff8e88960866f59c63a8
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2016-11-07 09:26:34 +0530

    Added vcs site link to make it easy to create bitbucket link to source

---
+++ README.md
@@ -0,0 +1,9 @@
+Some useful Sublime extentions I've written
+===========================================
+
+Commands
+--------
+
+### Sidharth: VCS Site Link
+
+The command pushes a link to the bit bucket page for the currnent file into the clipboard. The link includes the cursor position information.
---
+++ vcs_site_link.py
@@ -0,0 +1,108 @@
+import sublime_plugin
+import os.path
+import re
+import sublime
+
+
+class VcsSiteLinkCommand(sublime_plugin.TextCommand):
+
+    def row(self, position):
+        return self.view.rowcol(position)[0] + 1
+
+    def run(self, edit):
+        filename = self.view.file_name()  
+        selection = self.view.sel()
+
+        if not filename:
+            return
+
+        if not len(selection):
+            return 
+
+        region = self.view.sel()[0]
+        b = self.row(region.begin())
+        e = self.row(region.end())
+
+
+        vcs_site = self.guess_vcs_site(filename)
+
+        if not vcs_site:
+            return
+
+        sublime.set_clipboard(vcs_site.link(filename, (b, e)))
+
+    def guess_vcs_site(self, filename):
+
+        def find_remote_origin(config):
+            for section, properties in config:
+                if section == 'remote "origin"':
+                    props = dict(properties)
+                    return props
+
+
+
+        git_dir = self.find_git_dir(filename)
+        if not git_dir:
+
+            return
+
+        config = parse_git_config(os.path.join(git_dir, '.git', 'config'))
+        ro_props = find_remote_origin(config)
+
+        git_url = ro_props['url']
+        if 'bitbucket' in git_url:
+            project_path = git_url.split(':')[1]
+            return BitbucketSite(git_dir, project_path)
+        else:
+            return
+
+
+    def find_git_dir(self, filename):
+        path = filename
+        while(path):
+            path = os.path.dirname(path)
+            if '.git' in os.listdir(path):
+                return path
+
+
+class BitbucketSite:
+    path_tpl = 'https://bitbucket.org/{}/src/master/{}#Line-{}:{}'
+
+    def __init__(self, git_dir, project_path):
+        self.git_dir = git_dir
+        self.project_path = project_path
+
+    def link(self, filename, lines):
+        b, e = lines
+        file_path = os.path.relpath(filename, start=self.git_dir)
+        return self.path_tpl.format(self.project_path, file_path, b, e)
+
+
+# Hacky config parser
+def parse_git_config(filename):
+    section_re = re.compile(r"\[([^]]*)\]")
+    property_re = re.compile(r"\s*([^=]+)=(.*)")
+    lines = open(filename).readlines()
+    def g():
+        it = iter(lines)
+
+        while True:
+            l = next(it).strip()
+            m = section_re.match(l)
+            if m:
+                section = m.group(1)
+                break
+
+        section_properties = []
+        while True:
+            l = next(it).strip()
+            if property_re.match(l):
+                m = property_re.match(l)
+                section_properties.append((m.group(1).strip(), m.group(2).strip()))
+            elif section_re.match(l):
+                yield (section, section_properties)
+                section = section_re.match(l).group(1)
+                section_properties = []
+
+        yield (section, section_properties)
+    return list(g())
